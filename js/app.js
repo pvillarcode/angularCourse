1b0da23da7a23be36f5c538f128477daf66ab295
unknown <B03339A@W5092987.ena.us.experian.local>
2014-09-20 22:13:14 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,129 @@
+(function(){
+    var app = angular.module('store',[]);
+    
+    app.controller('StoreController', function(){
+        this.products = gems;
+    });
+    
+    app.controller('PanelController', function(){
+        this.tab = 1;
+        
+        this.selectTab = function(setTab){
+            this.tab = setTab;
+        }
+        
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        }    
+    });
+    
+    app.controller('ReviewController', function() {
+        this.review = {};
+        
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        }
+    });
+    
+    app.directive('productTitle',function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'include/product-title.html'
+        };
+    });
+    
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 23.95,
+            description: '...'    ,
+            canPurchase: true,
+            soldOut:true,
+            images: [
+                {
+                    full:'img/full.jpg',
+                    thumb:'img/thumb.jpg'
+                }            
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this product",
+                    author: "joe@thomas" 
+                },
+                {
+                    stars: 3,
+                    body: "wena loco",
+                    author: "amier@gasppars" 
+                }, 
+                {
+                    stars: 6,
+                    body: "Martillo asdf",
+                    author: "ojde@sapar" 
+                }
+            ]
+        },
+        {
+            name: 'Item 2',
+            price: 232.95,
+            description: '...'    ,
+            canPurchase: false,
+            soldOut:true,
+            images: [
+                {
+                    full:'img/ic_launcher.png',
+                    thumb:'img/thumb.jpg'
+                }            
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this product",
+                    author: "joe@thomas" 
+                },
+                {
+                    stars: 3,
+                    body: "wena loco",
+                    author: "amier@gasppars" 
+                }, 
+                {
+                    stars: 6,
+                    body: "Martillo asdf",
+                    author: "ojde@sapar" 
+                }
+            ]
+        },
+        {
+            name: 'Item 3',
+            price: 244.95,
+            description: '...'    ,
+            canPurchase: true,
+            soldOut:true,
+            images: [
+                {
+                    full:'img/full.jpg',
+                    thumb:'img/thumb.jpg'
+                }            
+            ],
+            reviews: [
+                {
+                    stars: 5,
+                    body: "I love this product",
+                    author: "joe@thomas" 
+                },
+                {
+                    stars: 3,
+                    body: "wena loco",
+                    author: "amier@gasppars" 
+                }, 
+                {
+                    stars: 6,
+                    body: "Martillo asdf",
+                    author: "ojde@sapar" 
+                }
+            ]
+        }
+    ]
+    
+})();
